7850daa83d99237b3163721a8dd721064fc54cbb
unkellia <unkellia002@gmail.com>
2022-04-10 11:32:31 -0700

    kundwa

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,17 @@
+nclude <stdio.h>
+#include <stdlib.h>
+
+/**
+ *  * main - main block
+ *   */
+
+void main(){
+	int n
+
+		scanf("%d", &n)
+		if(n>0)
+			        printf(n "is positive\n");
+		else if(n == 0)
+			        printf(n "is zero\n");
+		else if(n<0)
+			        printf(n "is negative\n");
