5a696751800954d651ec5f97fd2134fd5d21562c
unkellia <unkellia002@gmail.com>
2022-04-10 18:44:58 -0700

    message

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,8 +3,8 @@
 /**
  * main - prints all single digit numbers of base
  * 10 starting from 0, followed by a new line
- * Return: Always 0 (success)
- * /
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
