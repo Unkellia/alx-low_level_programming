93303baa30a78790f032ebb3187c15ba48ba5dd4
unkellia <unkellia002@gmail.com>
2022-04-10 17:44:56 -0700

    message

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,15 +1,10 @@
-#include <stdlib.h>                                                                                                                    
-#include <time.h>                                                                                                                      
+#include <stdlib.h>                                                                                                            
+#include <time.h>                                                                                                              
 #include <stdio.h>                                                                                                                     
-                                                                                                                                       
-/**                                                                                                                                    
- * main - assigns a random number to int n everytime                                                                                   
- * it executes, and prints it                                                                                                          
- * Return: Always 0 (Success)                                                                                                          
- */                                                                                                                                    
-int main(void)                                                                                                                         
-{                                                                                                                                      
-        int n;                                                                                                                
+ /**                                                                                                                         
+ * main - assigns a random number to int n everytime                                                                             * it executes, and prints it       
+ * Return: Always 0 (Success)                                                                                                    */                                                                                                                                    
+int main(void)                                                                                                                  {                                                                                                                                      int n;                                                                                                                
 	
 	srand(time(0));                                                                                                        
 	n = rand() - RAND_MAX / 2;                                                                                             
