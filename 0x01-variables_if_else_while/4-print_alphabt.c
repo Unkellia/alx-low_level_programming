01044e93b0bcb0680254a77e1e86019980daac08
unkellia <unkellia002@gmail.com>
2022-04-10 18:37:46 -0700

    message

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,18 +3,19 @@
 /**
  * main - prints the alphabet in lowercase,
  * followed by a new line, except q and e
- * Return: Always 0 (success)
- * /
+ * Return: Aways 0 (Sucess)
+ */
 int main(void)
 {
-        char ch = 'a';
+	char ch = 'a';
 
-	while (ch <= 'z'
+	while (ch <= 'z')
 	{
-		 if (ch != 'e' && ch != 'q')
-		 {
-                         putchar(ch);					               }
-		 ch++;
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
 	}
 	putchar('\n');
 	return (0);
