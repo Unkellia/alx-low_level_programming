4414270fdaacf779bb624a59548e6ed026deba28
unkellia <unkellia002@gmail.com>
2022-04-12 17:15:27 -0700

    message

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,17 +19,18 @@ void times_table(void)
 				product = row * column;
 				tens = product / 10;
 				ones = product % 10;
-																				if (column == 0)
+				
+				if (column == 0)
 				{
-																					_putchar('0');
-																				}
+					_putchar('0');
+				}
 				else if (product < 10)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(ones + '0');
-																																				}
+				}
 				else
 				{
 					_putchar(',');
