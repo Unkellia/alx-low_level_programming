e7303a240225a2a85a18dee8ea87426fc0ece982
unkellia <unkellia002@gmail.com>
2022-04-14 14:08:41 -0700

    message

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,9 +1,9 @@
-nclude "main.h"
+#include "main.h"
 
 /**
- * print_line - draws a straight line in the terminal
- * @n: number of times the character _ should be printed
- */
+ *  * print_line - draws a straight line in the terminal
+ *   * @n: number of times the character _ should be printed
+ *    */
 void print_line(int n)
 {
 	if (n <= 0)
